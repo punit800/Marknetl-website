5ac7123e4e368e9bf456544dd53dff71bed8bd5f
punit <punitvishwakarma47@gmail.com>
2023-10-05 18:40:32 +0530

    final project

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// $('.counter').counterUp({
+//   delay: 10,
+//   time: 1000
+//   });
+
+
+
+// $('counters').each(function)(){
+//   const This = $(this);
+//   $({Count: This.text()}).animate(
+//     {
+//       Count: This.parent().attr("data-count")
+//       {
+//         duration: 2000,
+//         easing: "linear",
+//         step: function(){
+//           step: function(){
+//             this.text(Math.floor(this.Count))
+//           },
+//           complete: function(){
+//             This.text(this.Count)
+//           }
+//         }
+//       }
+//     }
+//   )
+// }
